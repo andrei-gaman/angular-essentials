d0d290b1cdb4eba093b51d4308281fc9958da3aa
Andrei Gaman <andrei.gaman994@gmail.com>
2021-09-09 17:38:35 +0300

    Listening to (User) Events

--- src/app/user.component.ts
+++ src/app/user.component.ts
@@ -2,9 +2,16 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-user',
-  template: `<p>Hello {{name}}!</p>
-    <p>I'm the user component</p> `,
+  template: `
+    <input type="text" (input)="onUserInput($event)" />
+    <p>Hello {{ name }}!</p>
+    <p>I'm the user component</p>
+  `,
 })
 export class UserComponent {
-  name = "Andrei";
+  name = 'Andrei';
+
+  onUserInput(event) {
+    this.name = event.target.value;
+  }
 }
