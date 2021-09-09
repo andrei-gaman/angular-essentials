9f3757742519065c3401f90674a809874db0f3b2
Andrei Gaman <andrei.gaman994@gmail.com>
2021-09-09 17:34:22 +0300

    Outputting Content via String Interpolation

--- src/app/user.component.ts
+++ src/app/user.component.ts
@@ -2,9 +2,9 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-user',
-  template: `<p>Hello</p>
+  template: `<p>Hello {{name}}!</p>
     <p>I'm the user component</p> `,
 })
 export class UserComponent {
- 
+  name = "Andrei";
 }
