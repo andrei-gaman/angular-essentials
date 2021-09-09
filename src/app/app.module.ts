900f49efebccb5e256ed886998e17554d7e41471
Andrei Gaman <andrei.gaman994@gmail.com>
2021-09-09 18:20:06 +0300

    First Assignment

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { UserComponent } from './user.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    UserComponent
+    UserComponent,
+    DashboardComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/dashboard/dashboard.component.ts
@@ -0,0 +1,21 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-dashboard',
+  templateUrl: './dashboard.component.html',
+  styleUrls: ['./dashboard.component.css']
+})
+export class DashboardComponent implements OnInit {
+
+  loadState: String = 'loading';
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+  onSwitchState(){
+    this.loadState = 'finished';
+  }
+
+}
