966e510178bf94230068deff7e778ae9121baccf
Andrei Gaman <andrei.gaman994@gmail.com>
2021-10-14 12:46:12 +0300

    Optimize imports

--- src/app/user.component.ts
+++ src/app/user.component.ts
@@ -1,6 +1,4 @@
-import { EventEmitter } from '@angular/core';
-import { Output } from '@angular/core';
-import { Component, Input } from '@angular/core';
+import { Component, EventEmitter, Input, Output } from '@angular/core';
 
 @Component({
   selector: 'app-user',
